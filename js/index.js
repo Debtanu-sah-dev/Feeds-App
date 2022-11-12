99fd0a430507b76816f84d15587083effd149495
Debtanu Sah <debtanucoder@gmail.com>
2022-11-12 20:01:29 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,321 @@
+// Stream server For Peer To Peer Connection
+
+const servers = {
+    iceServers: [{
+        urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302"
+        ]
+    }],
+    iceCandidatePoolSize: 10
+}
+
+//Global Stream
+
+let peerconnections = [];
+let localStream = null;
+
+// DOM Elements
+
+const video = document.querySelector('#video');
+const videoc = document.querySelector('#video_container');
+const link = document.querySelector('[data-link]');
+const list = document.querySelector('[data-list]');
+const copy = document.querySelector('[data-copy]');
+const camera = document.querySelector('[data-camera]');
+const fullBtn = document.querySelector('[data-fullscreen]');
+const connectlink = document.querySelector('[data-connectlink]');
+const physics_enable = document.querySelector('[data-physics]');
+const location_btn = document.querySelector('[data-location]');
+const error_msg = document.querySelector('[data-error]');
+const end = document.querySelector('[data-end]');
+let isCamerOn = false;
+let local_link_primitive = "";
+let prev_buffer = null;
+let isFullscreenCon = false;
+let datachannel_list=[];
+let voices = [];
+let motion_obj = {
+    ao:null,
+    bo:null,
+    go:null,
+    xa:null,
+    ya:null,
+    za:null,
+    xag:null,
+    yag:null,
+    zag:null,
+    xg:null,
+    yg:null,
+    zg:null,
+    i:null
+};
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+};
+
+function restrictPC(){
+    datachannel_list = datachannel_list.filter((element) => {
+        return element[3].iceConnectionState != 'disconnected'
+    })
+    peerconnections = peerconnections.filter((element) => {
+        return element.iceConnectionState != 'disconnected'
+    })
+    requestAnimationFrame(restrictPC)
+}
+
+function sendMotionToAllPeers(){
+    
+    for(i = 0; i < datachannel_list.length;i++){
+        if(datachannel_list[i][1].readyState == "open"){
+            datachannel_list[i][1].send(JSON.stringify(motion_obj))
+        }
+    }
+}
+
+function handleOrientation(event) {
+    motion_obj.ao = event.alpha;
+    motion_obj.bo = event.beta;
+    motion_obj.go = event.gamma;
+    sendMotionToAllPeers()
+}
+function handleMotion(event) {
+    motion_obj.xag = event.accelerationIncludingGravity.x;
+    motion_obj.yag = event.accelerationIncludingGravity.y;
+    motion_obj.zag = event.accelerationIncludingGravity.z;
+    motion_obj.xa = event.acceleration.x;
+    motion_obj.ya = event.acceleration.y;
+    motion_obj.za = event.acceleration.z;
+    motion_obj.i = event.interval;
+    motion_obj.zg = event.rotationRate.alpha;
+    motion_obj.xg = event.rotationRate.beta;
+    motion_obj.yg = event.rotationRate.gamma;
+    sendMotionToAllPeers()
+}
+
+physics_enable.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (
+        DeviceMotionEvent &&
+        typeof DeviceMotionEvent.requestPermission === "function"
+    ) {
+        DeviceMotionEvent.requestPermission();
+    }
+    window.addEventListener("devicemotion", handleMotion);
+    window.addEventListener("deviceorientation", handleOrientation);
+})
+
+location_btn.addEventListener("click", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            let buffer = {
+                lat:pos.coords.latitude,
+                lon:pos.coords.longitude
+            }
+
+            for(i = 0; i < datachannel_list.length;i++){
+                if(datachannel_list[i][2].readyState == "open"){
+                    datachannel_list[i][2].send(JSON.stringify(buffer))
+                }
+            }
+        });
+    }
+    else{
+        error_msg.innerHTML = "Geolocation is not supported by this browser or device.";
+    }
+
+})
+
+
+async function updateList(cameras){
+    list.innerHTML = "";
+    let arr = await cameras;
+    arr.map((e) => {
+        const option = document.createElement('option');
+        option.label = e.label;
+        option.value = e.deviceId;
+
+        return option;
+    }).forEach((cameraOption) => {
+        list.appendChild(cameraOption)
+    })
+
+}
+
+fullBtn.addEventListener("click", () => {
+    isFullscreenCon = !isFullscreenCon;
+    if(isFullscreenCon){
+        videoc.requestFullscreen();
+        fullBtn.querySelector("span").innerText = "fullscreen_exit"
+    }
+    else{
+        document.exitFullscreen();
+        fullBtn.querySelector("span").innerText = "fullscreen"
+    }
+})
+
+async function getCameras() {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    return devices.filter(device => device.kind === "videoinput")
+}
+
+camera.addEventListener("click", async function () {
+    localStream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: {
+            deviceId: list.value
+        }
+    });
+    
+    video.srcObject = localStream;
+    videoc.style.display = "flex";
+    video.play();
+
+    isCamerOn = true;
+    connectlink.disabled=false
+})
+
+list.addEventListener("change", async function () {
+    camera.click();
+});
+
+async function connect(update,PeerConnection) {
+    if(isCamerOn){
+        update ? (connectlink.style.display = "none"):null;
+        const feedDoc = db.collection("feeds").doc();
+        const offer = feedDoc.collection("offer");
+        const candidate = feedDoc.collection("candidate");
+        const newp = feedDoc.collection("newp");
+        update ? (link.innerText = feedDoc.id):null;
+        localStream.getTracks().forEach(track => {
+            PeerConnection.addTrack(track, localStream);
+        });
+
+        PeerConnection.onicecandidate = event => {
+            event.candidate && offer.add(event.candidate.toJSON())
+        }
+        const offerDescriptor = await PeerConnection.createOffer();
+        await PeerConnection.setLocalDescription(offerDescriptor);
+    
+        newp.onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach(async (change) => {
+                if(change.type === "added"){
+                    let pc = new RTCPeerConnection(servers);
+                    peerconnections.push(pc)
+                    let dataChannel = pc.createDataChannel("message");
+                    let motionChannel = pc.createDataChannel("motion");
+                    let glChannel = pc.createDataChannel("gl");
+                    datachannel_list.push([dataChannel,motionChannel,glChannel,pc])
+                    dataChannel.addEventListener("message", (event) => {
+                        console.log(event)
+                        let speech = new SpeechSynthesisUtterance();
+                        speech.lang = "en";
+                        speech.text = event.data;
+                        speech.voice = voices[0];
+                        window.speechSynthesis.speak(speech);
+                    })
+                    
+                    let val = await connect(false,pc);
+
+                    newp.doc(change.doc.id).set({
+                        id:val,
+                    })
+                }
+            })
+        })
+
+        await feedDoc.set({
+            offer:{
+                sdp: PeerConnection.localDescription.sdp,
+                type: PeerConnection.localDescription.type
+            },
+            full:false
+        });
+
+        feedDoc.onSnapshot(async (snapshot) => {
+            const data = snapshot.data();
+            if(!PeerConnection.currentRemoteDescription && data?.answer){
+                let remDesc = new RTCSessionDescription(data.answer);
+                await PeerConnection.setRemoteDescription(remDesc);
+            }
+        })
+
+        candidate.onSnapshot(async (snapshot) => {
+            snapshot.docChanges().forEach(async (change) => {
+                if(change.type === "added"){
+                    let ICE = new RTCIceCandidate(change.doc.data());
+                    await PeerConnection.addIceCandidate(ICE)
+                }
+            })
+        })
+
+        return feedDoc.id
+    }
+    else{
+        alert("Please open your camera first");
+        camera.click()
+    }
+}
+
+connectlink.addEventListener("click",async function () {
+    let pc = new RTCPeerConnection(servers);
+    let dataChannel = pc.createDataChannel("message");
+    let motionChannel = pc.createDataChannel("motion");
+    let glChannel = pc.createDataChannel("gl");
+    datachannel_list.push([dataChannel,motionChannel,glChannel,pc])
+    dataChannel.addEventListener("message", (event) => {
+        console.log(event)
+        let speech = new SpeechSynthesisUtterance();
+        speech.lang = "en";
+        speech.text = event.data;
+        speech.voice = voices[0];
+        window.speechSynthesis.speak(speech);
+    })
+
+    peerconnections.push(pc)
+    local_link_primitive = await connect(true,pc);
+    end.style.display = "inline-block";
+    restrictPC();
+})
+
+end.addEventListener("click",async function () {
+    let doc = db.collection("feeds").doc(local_link_primitive);
+    let deleteabledocs = [doc];
+    let newp = doc.collection("newp");
+    if(newp){
+        let alldocs = await newp.get();
+    
+        alldocs.docs.forEach(async(docs) => {
+            let data = docs.data();
+            deleteabledocs.push(db.collection("feeds").doc(data.id))
+        })
+    }
+
+    deleteabledocs.forEach(async(docs) => {
+        await docs.delete();
+    })
+
+    peerconnections.forEach(pc => {
+        pc.close();
+    })
+    peerconnections = [];
+    localStream.getTracks().forEach(track => {
+        track.stop();
+    })
+    localStream = null;
+    isCamerOn = false;
+    link.innerText = "";
+    end.style.display = "none";
+    local_link_primitive = "";
+
+    video.srcObject = null;
+    connectlink.style.display = "inline-block";
+    location.reload()
+})
+
+const newList = getCameras();
+updateList(newList);
---
+++ js/remote.js
@@ -0,0 +1,267 @@
+// Stream server For Peer To Peer Connection
+
+const servers = {
+    iceServers: [{
+        urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302"
+        ]
+    }],
+    iceCandidatePoolSize: 10
+}
+
+//Global Stream
+
+let PeerConnection = new RTCPeerConnection(servers);
+let remoteStream = new MediaStream();
+
+
+//DOM Elements
+
+const video = document.querySelector('#video');
+const videoc = document.querySelector('#video-container');
+const link = document.querySelector('[data-link]');
+const fullBtn = document.querySelector('[data-fullscreen]');
+const connecter = document.querySelector('[data-connecter]');
+const connecter_wrap = document.querySelector('#connecter');
+const announce_input = document.querySelector('[data-aninp]');
+const announce_btn = document.querySelector('[data-anbtn]');
+const announce_panel = document.querySelector('#announce-panel');
+const motion_info = document.querySelector("#motion_obj");
+const geolocation_con = document.querySelector("#Geolocation");
+const elevation = document.querySelector("#elevation");
+const vrbtn = document.querySelector("[data-vr]");
+const bufvideo = document.querySelector("#bufvideo");
+const motion_toogle = document.querySelector("[data-motion-toogle]");
+const location_toogle = document.querySelector("[data-location-toogle]");
+let isVr = false;
+let isFullscreenCon = false;
+let isMotion = false;
+let isLocation = false;
+let dataChannel;
+let motionChannel;
+let glChannel;
+let motion_obj = null;
+let gl_buffer = null;
+let map_markers = []
+let map = L.map('Geolocation').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+motion_toogle.addEventListener("click", () => {
+    isMotion = !isMotion;
+    if(!isMotion){
+        motion_info.style.zIndex = "-10"
+    }
+    else if(isMotion){
+        motion_info.style.zIndex = "10000"
+    }
+})
+location_toogle.addEventListener("click", () => {
+    isLocation = !isLocation;
+    if(!isLocation){
+        geolocation_con.style.zIndex = "-10"
+    }
+    else if(isLocation){
+        geolocation_con.style.zIndex = "10000"
+    }
+})
+
+PeerConnection.addEventListener("datachannel", (e) => {
+    if(e.channel.label == "message"){
+        dataChannel = e.channel;
+    }
+    if(e.channel.label == "motion"){
+        motionChannel = e.channel;
+
+        motionChannel.addEventListener("message", (e) => {
+            motion_obj = JSON.parse(e.data);
+        })
+    }
+    if(e.channel.label == "gl"){
+        glChannel = e.channel;
+
+        glChannel.addEventListener("message",async function (e)  {
+            let data = JSON.parse(e.data)
+            map.setView([data.lat,data.lon],13);
+            let marker = L.marker([data.lat,data.lon]).addTo(map);
+            map_markers.push(marker)
+            let elevation_buffer = await fetchElevation(data.lat,data.lon);
+            let p = 2;
+            elevation.innerText = `Elevation: ${Math.round((elevation_buffer.results[0].elevation * 3.28084)*(10**p))/(10**p)} ft`
+        })
+    }
+})
+
+async function fetchElevation(lat, lon) {
+    let response = await fetch("https://api.open-elevation.com/api/v1/lookup?locations=" + lat + "," + lon)
+    let elevation_buffer = await response.json();
+    return elevation_buffer;
+}
+
+announce_btn.addEventListener("click", () => {
+    if(announce_input.value.trim() != ""){
+        dataChannel.send(announce_input.value)
+    }
+})
+
+vrbtn.addEventListener("click", () => {
+    isVr = !isVr;
+    if(isVr){
+        bufvideo.srcObject = remoteStream;
+        video.classList.add("full");
+        bufvideo.classList.add("full");
+        bufvideo.style.display = "flex"
+        videoc.requestFullscreen()
+        fullBtn.style.display = "none"
+    }
+    else{
+        video.classList.remove("full");
+        bufvideo.classList.remove("full");
+        bufvideo.srcObject = null;
+        document.exitFullscreen();
+        fullBtn.style.display = "flex"
+    }
+})
+
+document.addEventListener('fullscreenchange', onFullScreenChange, false);
+document.addEventListener('webkitfullscreenchange', onFullScreenChange, false);
+document.addEventListener('mozfullscreenchange', onFullScreenChange, false);
+
+function onFullScreenChange() {
+    var fullscreenElement = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
+
+    if(fullscreenElement == null){
+        vrbtn.style.display = "flex"
+        fullBtn.style.display = "flex"
+        fullBtn.querySelector("span").innerText = "fullscreen"
+        video.classList.remove("full");
+        bufvideo.style.display = "none"
+        bufvideo.classList.remove("full");
+        bufvideo.srcObject = null;
+        isVr=false;
+        isFullscreenCon = false;
+    }
+}
+
+fullBtn.addEventListener("click", () => {
+    isFullscreenCon = !isFullscreenCon;
+    if(isFullscreenCon){
+        videoc.requestFullscreen();
+        fullBtn.querySelector("span").innerText = "fullscreen_exit"
+        vrbtn.style.display = "none"
+        video.classList.add("entire")
+    }
+    else{
+        document.exitFullscreen();
+        fullBtn.querySelector("span").innerText = "fullscreen"
+        vrbtn.style.display = "flex"
+        video.classList.remove("entire")
+    }
+})
+
+PeerConnection.ontrack = event => {
+    event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track);
+    });
+}
+
+async function connect(feedId = link.value) {
+    try {
+        const feedDoc = db.collection("feeds").doc(feedId);
+        const offer = feedDoc.collection("offer");
+        const answer = feedDoc.collection("candidate");
+        const newp = feedDoc.collection("newp");
+        let callData = await feedDoc.get();
+        if(callData.data().full == false){
+
+            video.srcObject = remoteStream;
+    
+            PeerConnection.onicecandidate = event => {
+                event.candidate && answer.add(event.candidate.toJSON())
+            }
+    
+    
+            const offerer = new RTCSessionDescription(callData.data().offer);
+            await PeerConnection.setRemoteDescription(offerer);
+    
+            let answerer = await PeerConnection.createAnswer();
+            await PeerConnection.setLocalDescription(answerer);
+            feedDoc.update({
+                answer:{
+                    sdp: answerer.sdp,
+                    type: answerer.type
+                },
+                full:true
+            })
+    
+            offer.onSnapshot(async function (snapshot) {
+                snapshot.docChanges().forEach(async function (change) {
+                    if (change.type === "added") {
+                        const candidate = new RTCIceCandidate(change.doc.data());
+                        await PeerConnection.addIceCandidate(candidate);
+                    }
+                })
+            })
+    
+            // if(feedDoc){
+            //     throw new Error();
+            // }
+            // connecter.style.display = "none";
+            // let remDesc = new RTCSessionDescription(await feedDoc.get().data().sdp);
+            // await PeerConnection.setRemoteDescription(remDesc);
+            // let answer = await PeerConnection.createAnswer();
+            // await PeerConnection.setLocalDescription(answer);
+        }
+        else{
+            let doc = newp.doc();
+
+            doc.set({})
+            
+            doc.onSnapshot((snapshot) => {
+                const data = snapshot.data();
+                if(!PeerConnection.currentRemoteDescription && data.id != null){
+                    connect(data.id);
+                }
+            })
+        }
+
+        announce_panel.style.display = "flex";
+
+        return true;
+    } catch (e) {
+        console.error(e);
+        alert("Invalid Link");
+        return false;
+    }
+}
+
+connecter.addEventListener("click", async () => {
+    let bool = await connect();
+    if(bool){
+        connecter_wrap.style.display = "none";
+        videoc.style.display = "flex";
+    }
+});
+
+PeerConnection.addEventListener("connectionstatechange",(e) => {
+    if(PeerConnection.connectionState == "disconnected"){
+        endsession();
+    }
+})
+
+function endsession(){
+    alert("Disconnected");
+    location.reload()
+}
+
+//Initialize link
+if (new URLSearchParams(location.search).get("c") != null) {
+    link.value = new URLSearchParams(location.search).get("c");
+    connect();
+}
